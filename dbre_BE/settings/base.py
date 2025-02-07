5c1eb4ac7c4d86a8f343388a66acd66ce56c2324
mountain-kangkang <rngl10@naver.com>
2025-02-07 15:33:05 +0900

    [🚀Deploy] 환경분리 완?

--- dbre_BE/settings/base.py
+++ dbre_BE/settings/base.py
@@ -6,9 +6,14 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 
-load_dotenv()
+# 환경별 .env 파일 로드
+DJANGO_ENV = os.getenv('DJANGO_ENV', 'local')
+env_file = f'.env.{DJANGO_ENV}'
+load_dotenv(env_file)
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+DEBUG = os.getenv('DEBUG', 'True')
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
@@ -136,6 +141,18 @@ SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("Bearer",),
     "USER_ID_FIELD": "email",
     "USER_ID_CLAIM": "email",
+}
+
+# 공통 데이터베이스 설정
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
+    }
 }
 
 # redis 캐시 설정
--- dbre_BE/settings/prod.py
+++ dbre_BE/settings/prod.py
@@ -1,38 +1,11 @@
-import os
-
 from .base import *
 
 
-DEBUG = False
-
-# ALLOWED_HOSTS = ["your-domain.com"]   # 배포 시
-#
-# CSRF_COOKIE_HTTPONLY = True
-# CSRF_COOKIE_SECURE = True
-
-# -------------------------- 개발환경
+# ALLOWED_HOSTS = ["your.domain.com"]
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-]
+CSRF_COOKIE_HTTPONLY = True
+CSRF_COOKIE_SECURE = True
 
-CSRF_COOKIE_HTTPONLY = False
-CSRF_COOKIE_SECURE = False
-
-# -------------------------- 개발환경
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("DEPLOY_POSTGRES_HOST"),
-        "PORT": os.getenv("DEPLOY_POSTGRES_PORT"),
-    }
-}
-
-# SECURE_SSL_REDIRECT = True        # 배포 시
-# SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
