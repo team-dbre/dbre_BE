69595579833fe79bdada2da46170b665d2b600fb
limsu1015 <tngur1015@gmail.com>
2025-02-18 00:17:41 +0900

    ✨Feat : 포트원 정기 결제를 위한 스케줄러 설치
    장고 스케줄러 apscheduler 를 이용한 정기 결제 스케줄 기능 구현

--- dbre_BE/settings/base.py
+++ dbre_BE/settings/base.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount",
     "allauth.socialaccount.providers.google",
     "admin_api",
+    "apscheduler",
 ]
 
 
--- payment/apps.py
+++ payment/apps.py
@@ -1,6 +1,17 @@
+import logging
+
 from django.apps import AppConfig
+
+
+logger = logging.getLogger(__name__)
 
 
 class PaymentConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "payment"
+
+    def ready(self) -> None:
+        from payment.scheduler import start
+
+        logger.info("PaymentConfig.ready() 실행됨")
+        start()
