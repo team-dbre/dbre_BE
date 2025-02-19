a6f6fb79fffe8ff636e57186c35461f90af7852f
limsu1015 <tngur1015@gmail.com>
2025-02-19 15:52:23 +0900

    🔨Refactor : 구독 정보 조회 수정
    구독 정보 조회시 유저에서 sub_status도 같이 불러오게 수정

--- subscription/serializers.py
+++ subscription/serializers.py
@@ -4,9 +4,14 @@ from .models import SubHistories, Subs
 
 
 class SubsSerializer(serializers.ModelSerializer):
+    sub_status = serializers.SerializerMethodField()
+
     class Meta:
         model = Subs
         fields = "__all__"
+
+    def get_sub_status(self, obj):
+        return obj.user.sub_status
 
 
 class SubHistorySerializer(serializers.ModelSerializer):
