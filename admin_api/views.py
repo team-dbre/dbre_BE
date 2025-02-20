0c2f776711ca7a106a4bf3220a8127d6997f0b78
limsu1015 <tngur1015@gmail.com>
2025-02-20 16:34:09 +0900

    🚧WIP : admin 개발중
    대시보드 serializer

--- admin_api/views.py
+++ admin_api/views.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.core.cache import cache
 from django.db.models import Count, Max, Min, OuterRef, Q
 from django.utils import timezone
+from django.utils.timezone import now
 from drf_spectacular.utils import OpenApiExample, OpenApiResponse, extend_schema
 from rest_framework import serializers, status
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
@@ -17,6 +18,7 @@ from admin_api.models import AdminLoginLog
 from admin_api.serializers import (
     AdminLoginSerializer,
     AdminUserSerializer,
+    DashboardSerializer,
     SubscriptionHistorySerializer,
     SubscriptionSerializer,
 )
@@ -62,33 +64,42 @@ class DashboardView(APIView):
     대시 보드
     """
 
+    permission_classes = [IsAdminUser]
+
     def get(self, request: Request) -> Response:
-        total_subscriptions = Subs.objects.count()
-        active_subscriptions = Subs.objects.filter(auto_renew=True).count()
-        paused_subscriptions = Subs.objects.filter(auto_renew=False).count()
+        # 전체 구독자 수
+        total_subscriptions = Subs.objects.exclude(
+            user__sub_status__in=[None, "cancelled"]
+        ).count()
+        # 오늘 일시정지 수
+        paused_subscriptions = (
+            SubHistories.objects.filter(status="paused", change_date__date=now().date())
+            .values("user")
+            .distinct()
+            .count()
+        )
+        # 신규 구독 수
         new_subscriptions_today = Subs.objects.filter(
             start_date__date=timezone.now().date()
         ).count()
-        changed_today = Subs.objects.filter(
-            next_bill_date__date=timezone.now().date()
-        ).count()
+        # active_subscriptions = Subs.objects.filter(auto_renew=True).count() # 진행 중
 
-        return Response(
-            {
-                "total_subscriptions": total_subscriptions,  # 전체 구독
-                "active_subscriptions": active_subscriptions,  # 진행 중
-                "paused_subscriptions": paused_subscriptions,  # 일시 정지
-                "new_subscriptions_today": new_subscriptions_today,  # 오늘 신규 구독
-                "changed_today": changed_today,  # 오늘 상태 변경
-            },
-            status=status.HTTP_200_OK,
-        )
+        data = {
+            "total_subscriptions": total_subscriptions,  # 전체 구독
+            # "active_subscriptions": active_subscriptions,  # 진행 중
+            "paused_subscriptions": paused_subscriptions,  # 일시 정지
+            "new_subscriptions_today": new_subscriptions_today,  # 오늘 신규 구독
+        }
+
+        serializer = DashboardSerializer(instance=data)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @extend_schema(tags=["admin"])
 class SubscriptionListView(APIView):
     """
-    구독 조회
+    구독 현황 관리
     """
 
     def get(self, request: Request) -> Response:
