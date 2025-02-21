5a4ef520777217251434ab6353aefd7b6cd7848d
mountain-kangkang <rngl10@naver.com>
2025-02-21 14:16:44 +0900

    [🚀Deploy] 끝...이 올까?
    ...ㅜ

--- dbre_BE/settings/local.py
+++ dbre_BE/settings/local.py
@@ -21,3 +21,7 @@ INSTALLED_APPS += ["debug_toolbar"]
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+if DEBUG:
+    INSTALLED_APPS += ['debug_toolbar']
+    MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
